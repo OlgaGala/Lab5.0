980f8f2502989626e0bc0c181a963618a019c43d
OlgaGala <olechkagala@gmail.com>
2021-05-06 21:33:39 +0300

    Implement Executor class, add Executor builder

--- src/main/java/com/olga/util/Executor.java
+++ src/main/java/com/olga/util/Executor.java
@@ -1,38 +1,41 @@
 package com.olga.util;
 
-import com.olga.dragon.Color;
-import com.olga.dragon.DragonCharacter;
+import com.olga.command.Exit;
+import com.olga.command.manager.CommandManager;
+import com.olga.dragon.Dragon;
+import com.olga.i18n.Messenger;
+import com.olga.print.api.Formatter;
+import com.olga.print.api.Printer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
 
-/**
- * Класс, предоставляющий пользовательский терминал для работы с коллекцией (через Service)
- * Все пользовательские команды поступают сюда, а затем вызывается соответствующи метод Service
- */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Executor {
 
-    /**
-     * Класс управления коллекцией, которым мы будем управлять в терминале
-     */
-    private final CollectionService collectionService;
+    private Stack<Dragon> mDataSet;
+    private CommandManager commandManager;
+    private Formatter formatter;
+    private Printer printer;
+    private Messenger messenger;
 
-    /**
-     * Источник ввода, от него зависит откуда такие методы как add, update и др. будут считывать элементы
-     */
-    private UserInput userInput;
-
-    public Executor(CollectionService collectionService) {
-        this.collectionService = collectionService;
-        this.userInput = new ConsoleUserInput();
+    private void init() {
+        commandManager.setMessenger(messenger);
+        commandManager.setFormatter(formatter);
+        commandManager.setPrinter(printer);
+        commandManager.setMDataSet(mDataSet);
     }
 
-    /**
-     * Метод, запускающий терминал
-     * Непрерывно слушает пользовательский ввод, вызывает соответствующую команду и выводит результат
-     */
     public void start() {
+
+        init();
+
         System.out.println("Вас приветствует терминал управления списком пользователей.\n" +
                 "Введите одну из команд для продолжения (help - список доступных команд)");
 
@@ -43,8 +46,8 @@ public class Executor {
                 // Вызываем команду и выводим результат
                 // В случае, если метод execute_command бросил исключение,
                 // оно обрабатывается, а программа продолжает работу.
-                String result = execute_command(response);
-                System.out.println(result);
+                commandManager.executeUserCommand(response);
+
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
@@ -52,144 +55,6 @@ public class Executor {
 
         // После того, как пользователь ввёл exit,
         // необходимо вызвать соответствующий метод Service и закончить выполнение программы
-        collectionService.exit();
+        new Exit(null).execute(null);
     }
-
-    /**
-     * Метод вызывает методы Service в соответствии с пользовательским вводом
-     * @param response - команда + аргументы, которые ввел пользователь
-     * @return - Результат выполнения
-     * @throws Exception - Некоторые методы бросают Exception,
-     * необходимо передать их на более высокий уровень и обработать уже там (В методе start())
-     */
-    public String execute_command(String response) throws Exception {
-
-        // Разделяем пользовательский ввод на команду и аргументы
-        String[] array = response.split(" ");
-        String command = array[0];
-
-        // Аргументов должно быть либо 0, либо 1
-        String param = array.length > 1 ? array[1] : null;
-
-        switch (command) {
-            case "help": return collectionService.help();
-            case "info": return collectionService.info();
-            case "show": return collectionService.show();
-            case "add": return collectionService.add(userInput.enterElement());
-            case "clear": return collectionService.clear();
-            case "save": return collectionService.save();
-            case "shuffle": return collectionService.shuffle();
-
-            case "insert_at": return isNumeric(param)
-                    ? collectionService.insert_at(Integer.parseInt(param), userInput.enterElement())
-                    : "Некорректные параметры для команды insert_at";
-
-            case "remove_at": return isNumeric(param)
-                    ? collectionService.remove_at(Integer.parseInt(param))
-                    : "Некорректные параметры для команды remove_at";
-
-            case "remove_all_by_cave":
-                return collectionService.remove_all_by_cave(userInput.enterCave());
-
-            case "filter_less_than_character":
-                return collectionService.filter_less_than_character(DragonCharacter.valueOf(param));
-
-            case "filter_greater_than_color":
-                return collectionService.filter_greater_than_color(Color.valueOf(param));
-
-            // В каждом случае мы проверяем валидность аргументов и,
-            // либо вызываем метод, либо возвращаем результат о некорректном вводе
-            case "update_id": return isNumeric(param)
-                    ? collectionService.update_id(Integer.parseInt(param), userInput.enterElement())
-                    : "Некорректные параметры для команды update_id";
-            case "remove_by_id": return isNumeric(param)
-                    ? collectionService.remove_by_id(Integer.parseInt(param))
-                    : "Некорректные параметры для команды remove_by_id";
-            case "execute_script": return param != null
-                    ? execute_script(param) :
-                    "Для выполнения необходимо имя файла";
-
-            default: return "Некорректный ввод. Пожалуйста, введите команду еще раз";
-        }
-    }
-
-    /**
-     * Метод проверяет, является ли строка числом
-     * @return - true, если является
-     */
-    private static boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            // Здесь мы преобразуем строку в Double, так как это самый большой тип,
-            // А также потому что любой числовой тип можно неявно преобразовать в Double
-            Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            // В случае, если строка не является числом, то мы просто возвращаем false
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Метод выполняет чтения файла-скрипта
-     * Построчно считывает команды с файла и посылает их execute_command()
-     * @param fileName - имя файла скрипта
-     * @return - Результат работы всего скрипта
-     * @throws Exception - В случае отсутствия файла или ошибок в скрипте
-     */
-    public String execute_script(String fileName) throws Exception {
-
-        if(!new File(fileName).exists()) throw new IOException("Файл не найден");
-
-        try(Scanner scanner = new Scanner(new File(fileName))) {
-
-            // Перед выполнением скрипта меняем источник ввода объектов на файловый
-            setFileSource(scanner);
-            StringBuilder result = new StringBuilder();
-
-            String response;
-            while (scanner.hasNextLine()) {
-
-                response = scanner.nextLine();
-
-                // Обязательно нужно проверить условие зацикливания, чтобы скрипт не вызвал сам себя
-                if(response.equals("execute_script " + fileName)) {
-                    result.append("Нельзя вызывать файл из самого себя!" + "\n");
-                } else {
-                    result.append(execute_command(response)).append("\n");
-                }
-            }
-
-            return result.toString();
-
-        } catch (IOException e) {
-            throw new Exception(e.getMessage());
-        } catch (Exception e) {
-            throw new Exception("Скрипт некорректный");
-        } finally {
-            // В конце снова меняем источник ввода объектов на консольный
-            setConsoleSource();
-        }
-    }
-
-
-    /**
-     * Метод, переназначающий источник ввода на файловый
-     * После вызова этого метода, элементы будут считываться с файла, а не с консоли
-     * @param in - Поток ввода
-     */
-    void setFileSource(Scanner in) {
-        userInput = new FileUserInput(in);
-    }
-
-    /**
-     * Метод, переназначающий источник ввода на консольный
-     * После вызова этого метода, элементы будут считываться с консоли
-     */
-    void setConsoleSource() {
-        userInput = new ConsoleUserInput();
-    }
-
 }
---
+++ src/main/java/com/olga/util/ExecutorBuilder.java
@@ -0,0 +1,50 @@
+package com.olga.util;
+
+import com.olga.command.manager.CommandManager;
+import com.olga.dragon.Dragon;
+import com.olga.i18n.Messenger;
+import com.olga.print.api.Formatter;
+import com.olga.print.api.Printer;
+import lombok.NoArgsConstructor;
+
+import java.util.Stack;
+
+@NoArgsConstructor
+public class ExecutorBuilder {
+
+    private Stack<Dragon> mDataSet;
+    private CommandManager commandManager;
+    private Formatter formatter;
+    private Printer printer;
+    private Messenger messenger;
+
+    public ExecutorBuilder setDataSet(Stack<Dragon> mDataSet) {
+        this.mDataSet = mDataSet;
+        return this;
+    }
+
+    public ExecutorBuilder setCommandManager(CommandManager commandManager) {
+        this.commandManager = commandManager;
+        return this;
+    }
+
+    public ExecutorBuilder setFormatter(Formatter formatter) {
+        this.formatter = formatter;
+        return this;
+    }
+
+    public ExecutorBuilder setPrinter(Printer printer) {
+        this.printer = printer;
+        return this;
+    }
+
+    public ExecutorBuilder setMessenger(Messenger messenger) {
+        this.messenger = messenger;
+        return this;
+    }
+
+    public Executor build() {
+        return new Executor(mDataSet, commandManager, formatter, printer, messenger);
+    }
+
+}
