a19065c7db6706dacad4c9a58846f3b8f7cc8dbb
OlgaGala <olechkaGala@gmail.com>
2021-06-27 18:48:40 +0300

    Add util classes, delete unused methods

--- client/src/main/java/com/client/ClientExecutor.java
+++ client/src/main/java/com/client/ClientExecutor.java
@@ -16,43 +16,6 @@ import java.util.Scanner;
 @AllArgsConstructor
 public class ClientExecutor {
 
-    private final Client client;
-    private final Printer printer;
-
-    public void start() throws Exception {
-
-        User user = client.auth();
-
-        System.out.println("Приветствие");
-
-        Scanner sc = new Scanner(System.in);
-        String response;
-        while (!(response = sc.nextLine()).equals("exit")) {
-            try {
-                // Вызываем команду и выводим результат
-                // В случае, если метод execute_command бросил исключение,
-                // оно обрабатывается, а программа продолжает работу.
-                if (response.equals("")) {
-                    System.out.println("Пожалуйста, введите корректные данные");
-                } else {
-                    String[] array = response.split(" ");
-                    String commandName = array[0];
-
-                    MessageReq message = client.prepareRequest(response, validateAnnotation(Command.validateCommand(commandName), user));
-                    message.setUser(user);
-                    MessageResp result = client.sendRequest(message);
-
-                    printer.printResult(result.getResult());
-                }
-
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-
-        client.stop("Программа завершилась успешно");
-    }
-
     private Dragon validateAnnotation(Class<? extends Command> c, User user) throws Exception {
 
         if(c.isAnnotationPresent(AttachedObj.class)) {
@@ -62,9 +25,5 @@ public class ClientExecutor {
         }
 
         return null;
-    }
-
-    public static void main(String[] args) throws Exception {
-        new ClientExecutor(new Client(), new PrinterImpl()).start();
     }
 }
---
+++ client/src/main/java/com/client/util/CustomTableView.java
@@ -0,0 +1,94 @@
+package com.client.util;
+
+import com.api.entity.Dragon;
+import com.api.i18n.Messenger;
+import com.api.i18n.MessengerFactory;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Stack;
+import java.util.TimerTask;
+
+public class CustomTableView extends TimerTask {
+
+    private static final Messenger messenger = MessengerFactory.getMessenger();
+
+    private final Stack<Dragon> storageOld;
+
+    private final Stack<Dragon> storage;
+
+    private final  TableView<Dragon> tableView;
+
+    private final Painter painter;
+
+    public CustomTableView(GraphicsContext gc, GraphicsContext gc2, TableView<Dragon> tableView){
+        this.tableView = tableView;
+
+        this.storageOld = new Stack<>();
+        this.storage = new Stack<>();
+
+        this.painter = new Painter(gc, gc2, storageOld);
+
+        this.createTable();
+    }
+
+    @Override
+    public void run() {
+
+        // TODO connect to the server and get all available data
+
+//        storage.stream().forEach(x -> tableView.getItems().add(x));
+//        storage.stream().forEach(x -> drawHuman(x, x.getX(), x.getY(), x.getSize()));
+//        tableView.refresh();
+//        storageOld = storage;
+    }
+
+
+    public void createTable() {
+
+        TableColumn<Dragon, Dragon> idColumn = new TableColumn<>(messenger.getMessage("name"));
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+
+        TableColumn<Dragon, Dragon> nameColumn = new TableColumn<>(messenger.getMessage("name"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Dragon, Dragon> xColumn = new TableColumn<>("X");
+        xColumn.setCellValueFactory(new PropertyValueFactory<>("X"));
+
+        TableColumn<Dragon, Dragon> yColumn = new TableColumn<>("Y");
+        yColumn.setCellValueFactory(new PropertyValueFactory<>("Y"));
+
+        TableColumn<Dragon, Dragon> ageColumn = new TableColumn<>(messenger.getMessage("age"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+
+        TableColumn<Dragon, Dragon> colorColumn = new TableColumn<>(messenger.getMessage("color"));
+        colorColumn.setCellValueFactory(new PropertyValueFactory<>("color"));
+
+        TableColumn<Dragon, Dragon> typeColumn = new TableColumn<>(messenger.getMessage("type"));
+        typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+        TableColumn<Dragon, Dragon> characterColumn = new TableColumn<>(messenger.getMessage("character"));
+        characterColumn.setCellValueFactory(new PropertyValueFactory<>("character"));
+
+        TableColumn<Dragon, Dragon> depthColumn = new TableColumn<>(messenger.getMessage("depth"));
+        depthColumn.setCellValueFactory(new PropertyValueFactory<>("depth"));
+
+        TableColumn<Dragon, Dragon> numberOfTreasureColumn = new TableColumn<>(messenger.getMessage("numberOfTreasure"));
+        numberOfTreasureColumn.setCellValueFactory(new PropertyValueFactory<>("numberOfTreasure"));
+
+        TableColumn<Dragon, Dragon> creationDateColumn = new TableColumn<>(messenger.getMessage("creationDate"));
+        creationDateColumn.setCellValueFactory(new PropertyValueFactory<>("creationDate"));
+
+
+        tableView.getColumns().addAll(
+                idColumn, nameColumn, xColumn,
+                yColumn, ageColumn, characterColumn,
+                typeColumn, characterColumn, depthColumn,
+                numberOfTreasureColumn, creationDateColumn
+        );
+    }
+
+}
