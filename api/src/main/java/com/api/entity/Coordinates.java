2d9e20a77bf4c7a36536ad58095df4b869a9ee93
OlgaGala <olechkaGala@gmail.com>
2021-06-28 16:17:04 +0300

    Add lombok annotations

--- api/src/main/java/com/api/entity/Coordinates.java
+++ api/src/main/java/com/api/entity/Coordinates.java
@@ -1,38 +1,22 @@
 package com.api.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Objects;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Coordinates implements Serializable {
 
     @NotNull(message = "X can't be null")
     private Double x; // Поле не может быть null
     private int y;
-
-    public Coordinates(Double x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Coordinates() {
-    }
-
-    public Double getX() {
-        return x;
-    }
-
-    public void setX(Double x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
 
     @Override
     public boolean equals(Object o) {
